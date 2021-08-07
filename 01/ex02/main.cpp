48d8fa7c0f834dde459e7a62af2b3a85feb773ec
abdel-ke <kemmal1337@gmail.com>
2021-08-07 03:41:44 +0100

    ex02_done!!

---
+++ 01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdel-ke <abdel-ke@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/07 02:44:53 by abdel-ke          #+#    #+#             */
+/*   Updated: 2021/08/07 03:39:41 by abdel-ke         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string	str("HI THIS IS BRAIN");
+	std::string * stringPTR(&str);
+	std::string & stringREF(str);
+
+	std::cout	<< "Adresse of string:\t" << &str << std::endl
+				<< "Adresse of stringPTR:\t" << stringPTR << std::endl
+				<< "Adresse of stringREF:\t" << &stringREF << std::endl;
+	std::cout	<< "--------------------------------------" << std::endl;
+	
+	std::cout	<< "POINTER OF stringPTR:\t" << *stringPTR << std::endl
+				<< "REFERENCE OF stringREF:\t" << stringREF << std::endl;
+}
