f9cb4e7aeb68156076e98bbdec41614b24dcf6e9
abdel-ke <kemmal1337@gmail.com>
2021-08-07 01:54:06 +0100

    Done!!

--- ex00/newZombie.cpp
+++ ex00/newZombie.cpp
@@ -6,7 +6,7 @@
 /*   By: abdel-ke <abdel-ke@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/05 18:33:47 by abdel-ke          #+#    #+#             */
-/*   Updated: 2021/08/06 18:44:14 by abdel-ke         ###   ########.fr       */
+/*   Updated: 2021/08/07 01:45:35 by abdel-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 Zombie*	newZombie( std::string name )
 {
-	Zombie *z = new Zombie();
+	Zombie *z = new Zombie;
 
 	z->setzombie(name);
 	return (z);
