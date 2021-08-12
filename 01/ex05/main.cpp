65726fdc3fb41972ba5b339a07be7fe6c16af053
abdel-ke <kemmal1337@gmail.com>
2021-08-12 22:55:36 +0100

    Update_ex05

--- 01/ex05/main.cpp
+++ 01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abdel-ke <abdel-ke@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/12 00:45:35 by abdel-ke          #+#    #+#             */
-/*   Updated: 2021/08/12 00:50:41 by abdel-ke         ###   ########.fr       */
+/*   Updated: 2021/08/12 22:10:12 by abdel-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 int main(int ac, char **av)
 {
 	Karen Karen;
-	ac--;
-	Karen.complain(av[1]);
+	if (ac != 1)
+		Karen.complain(av[1]);
+	else
+		std::cout << "[ Probably complaining about insignificant problems ]\n";
 }
