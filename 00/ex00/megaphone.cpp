2fb6433e62f42555d6c23db8ff9c02210caf99cc
abdel-ke <kemmal1337@gmail.com>
2021-08-12 15:46:28 +0100

    update ex00

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: abdel-ke <abdel-ke@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 23:31:01 by abdel-ke          #+#    #+#             */
-/*   Updated: 2021/08/12 15:34:40 by abdel-ke         ###   ########.fr       */
+/*   Updated: 2021/08/12 15:39:01 by abdel-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int main(int ac, char **av)
 	if (ac != 1)
 	{
 		std::string	str(av[1]);
-		for (char c : str)
+		for (char &c : str)
 			std::cout << (char)toupper(c);
 	}
 	else
