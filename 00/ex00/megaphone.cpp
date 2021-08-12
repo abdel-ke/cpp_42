03925b53a80db30f769f3117ad36606d5bced892
abdel-ke <kemmal1337@gmail.com>
2021-08-12 15:30:15 +0100

    Update_ex00

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: abdel-ke <abdel-ke@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 23:31:01 by abdel-ke          #+#    #+#             */
-/*   Updated: 2021/07/30 00:27:36 by abdel-ke         ###   ########.fr       */
+/*   Updated: 2021/08/12 15:27:15 by abdel-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,9 @@ int main(int ac, char **av)
 {
 	if (ac != 1)
 	{
-		for (int i = 1; av[i]; i++)
-		{
-			for (int j = 0; av[i][j]; j++)
-				av[i][j] = toupper(av[i][j]);
-			std::cout << av[i];
-		}
+		std::string	str(av[1]);
+		for (char c : str)
+			std::cout << (char)toupper(c);
 	}
 	else
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
