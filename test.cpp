31a1f8a9e4b3c41826c279120cf20c3e5ea7c2b1
abdel-ke <kemmal1337@gmail.com>
2021-10-15 12:00:35 +0100

    .

--- test.cpp
+++ test.cpp
@@ -1,8 +1,47 @@
-#include <iostream>
+#include "test.hpp"
+
+test::test(int a, int b, int c): x(a), y(b), z(c)
+{
+
+}
+
+void test::geter()
+{
+	std::cout << x << " " << y << " " << z << std::endl;
+}
+
+bool test::operator==(test t)
+{
+	if (t.x == this->x)
+	{
+		std::cout << "TRUE\n";
+		return (true);
+	}
+	else 
+	{
+		std::cout << "FALSE\n";
+		return false;
+	}
+}
+
+test test::operator=(test t)
+{
+	if (t.x == this->x)
+	{
+		std::cout << "TRUE\n";
+		return t;
+	}
+	else 
+	{
+		std::cout << "FALSE\n";
+		return NULL;
+	}
+}
 
 int main()
 {
-	std::string str("hamid\n");
-	for (char c : str)
-		std::cout << (char)toupper(c);
+	test t1(0,1,2), t2(0, 5, 6);
+	t1 = t2;
+	t1.geter();
+	t2.geter();
 }
